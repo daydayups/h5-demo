7c47178568b4e7851af6170d8367fd02b38c3512
Xiaofan Zhang <zhangxf05@163.com>
2015-07-07 15:38:48 +0800

    3d visual

--- circleAnimate/js/spin03.js
+++ circleAnimate/js/spin03.js
@@ -4,7 +4,7 @@
 window.spin = function (config) {
   var base = document.querySelector(config.base),
       region = config.region,
-      item = Array.prototype.slice.call(document.querySelectorAll(config.item),0),//convert NodeList to Array
+      item = Array.prototype.slice.call(document.querySelectorAll(config.item), 0),//convert NodeList to Array
       itemSize = config.itemSize,
       itemColor = config.itemColor,
       center = config.center;
@@ -18,10 +18,10 @@ window.spin = function (config) {
   /** get Points */
 //  var points = [{x:"", y:""}];
   var //points = [],//store item position
-      deg = 360/item.length,//this degree will be used in position computing
-      radius = base.clientHeight > base.clientWidth ? (base.clientWidth - itemSize)/2 : (base.clientHeight - itemSize)/ 2,
-      centerX = base.clientWidth/2,
-      centerY = base.clientHeight/2;
+      deg = 360 / item.length,//this degree will be used in position computing
+      radius = base.clientHeight > base.clientWidth ? (base.clientWidth - itemSize) / 2 : (base.clientHeight - itemSize) / 2,
+      centerX = base.clientWidth / 2,
+      centerY = base.clientHeight / 2;
 
   console.info("centerX:" + centerX + " centerY:" + centerY);
 
@@ -31,7 +31,7 @@ window.spin = function (config) {
 //    points.push({x: centerX + radius*Math.sin((i+1)*deg*Math.PI/180) ,y: centerY - radius*Math.cos((i+1)*deg*Math.PI/180)});
 //  }
   /** Compatibility */
-  if(!Array.prototype.forEach) {
+  if (!Array.prototype.forEach) {
     Array.prototype.forEach = function (fun /*, thisp*/) {
       var len = this.length;
       if (typeof fun != "function")
@@ -53,8 +53,8 @@ window.spin = function (config) {
     element.style.height = itemSize + "px";
     element.style.borderRadius = "50%";
     element.style.position = "absolute";
-    element.style.left = (centerX - itemSize/2) + "px";
-    element.style.top = (centerY - itemSize/2) + "px";
+    element.style.left = (centerX - itemSize / 2) + "px";
+    element.style.top = (centerY - itemSize / 2) + "px";
 //    element.style.left = points[index].x + "px";
 //    element.style.top = points[index].y + "px";
     element.style.backgroundColor = itemColor;
@@ -63,10 +63,11 @@ window.spin = function (config) {
   //animate
   var d = deg;
   var speed = 1;
+
   function animate() {
     item.forEach(function (element, index, array) {
-      element.style.setProperty("transform", "translate("+ (radius*Math.sin(index*d*Math.PI/180 + speed/100)) + "px" + ","+
-          ( 0 - radius*Math.cos(index*d*Math.PI/180 + speed/100)) + "px" +")");
+      element.style.setProperty("transform", "translate(" + (radius * Math.sin(index * d * Math.PI / 180 + speed / 1000)) + "px" + "," +
+          ( 0 - radius * Math.cos(index * d * Math.PI / 180 + speed / 1000)) + "px" + ")");
     });
     speed++;
     if (speed === 1000) {
@@ -74,13 +75,14 @@ window.spin = function (config) {
     }
     requestAnimFrame(animate);
   }
+
   animate();
 
 }
 
 window.requestAnimFrame = function () {
   return window.requestAnimationFrame || window.webkitRequestAnimationFrame || window.mozRequestAnimationFrame || window.oRequestAnimationFrame || window.msRequestAnimationFrame || function (e) {
-    window.setTimeout(e, 1e3/60)
+    window.setTimeout(e, 1e3 / 60)
   }
 }(), window.cancelRequestAnimFrame = function () {
   return window.cancelAnimationFrame || window.webkitCancelRequestAnimationFrame || window.mozCancelRequestAnimationFrame || window.oCancelRequestAnimationFrame || window.msCancelRequestAnimationFrame || clearTimeout
